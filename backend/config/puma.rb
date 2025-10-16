81c3c8bee84144d34124a10e60c7ec56cb5df4f4
Andrii Skega <Game.a.s.production@gmail.com>
2025-10-16 14:32:02 +0200

    "Configure Rails for Render production deployment"

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -30,6 +30,10 @@ threads threads_count, threads_count
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
