f55b122e0c4d6e1a6246ff0bd9ea6b012506a8f2
Andrii Skega <Game.a.s.production@gmail.com>
2025-10-16 20:07:54 +0200

    "Fix CORS for embedded frontend"

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,12 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*',  'http://localhost:3000', 'http://localhost:5173' 
+    origins '*', 
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       # expose: ['Authorization'],
-      credentials: true
+      credentials: false
   end
 end
